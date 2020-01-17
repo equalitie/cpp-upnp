cb885727cc5b7a60fb44adc9dd24c9314814434e
Peter Jankuliak <p.jankuliak@gmail.com>
2020-01-17 17:47:34 +0100

    Use igd::get_generic_port_mapping_entry in example

--- example/add-port-mapping.cpp
+++ example/add-port-mapping.cpp
@@ -6,9 +6,34 @@ namespace net = upnp::net;
 
 const char* pad = "  ";
 
-void list_port_mappings(upnp::igd& igd, net::yield_context yield)
+ostream& operator<<(ostream& os, const upnp::igd::map_entry& e) {
+    return cerr << e.proto
+                << " EXT:" << e.ext_port
+                << " INT:" << e.int_port
+                << " ADDR:" << e.int_client
+                << " DURATION:" << e.lease_duration.count() << "s"
+                << " " << e.description;
+}
+
+void list_port_mappings_igd1(upnp::igd& igd, net::yield_context yield)
 {
-    cerr << "Getting list of port mappings\n";
+    cerr << "Getting list of port mappings IGD1\n";
+
+    for (unsigned i = 0; ; ++i) {
+        auto r = igd.get_generic_port_mapping_entry(i, yield);
+        if (!r) {
+            if (i == 0) {
+                cerr << pad << "Error: " << r.error() << "\n";
+            }
+            break;
+        }
+        cerr << pad << r.value() << "\n";
+    }
+}
+
+void list_port_mappings_igd2(upnp::igd& igd, net::yield_context yield)
+{
+    cerr << "Getting list of port mappings IGD2\n";
 
     auto r = igd.get_list_of_port_mappings( upnp::igd::udp
                                           , 0
@@ -17,15 +42,9 @@ void list_port_mappings(upnp::igd& igd, net::yield_context yield)
                                           , yield);
 
     if (r) {
-        cerr << pad << "Success, found " << r.value().size() << " entries:\n";
+        cerr << pad << "Found " << r.value().size() << " entries:\n";
         for (auto e : r.value()) {
-            cerr << pad
-                 << e.proto
-                 << " EXT:" << e.ext_port
-                 << " INT:" << e.int_port
-                 << " ADDR:" << e.int_client
-                 << " DURATION:" << e.lease_duration.count() << "s"
-                 << " " << e.description << "\n";
+            cerr << pad << e << "\n";
         }
     } else {
         cerr << pad << "Error: " << r.error() << "\n";
@@ -102,23 +121,25 @@ int main()
             cerr << pad << igd.friendly_name() << "\n";
 
             get_external_address(igd, yield);
+
             add_port_mapping(igd, 7777, socket.local_endpoint().port(), yield);
-            list_port_mappings(igd, yield);
+            list_port_mappings_igd1(igd, yield);
+            list_port_mappings_igd2(igd, yield);
+
             //delete_port_mapping(igd, 7777, yield);
-            //list_port_mappings(igd, yield);
         }
 
-        cerr << "Listening on UDP " << socket.local_endpoint() << "\n";
+        //cerr << "Listening on UDP " << socket.local_endpoint() << "\n";
 
-        while (true) {
-            upnp::error_code ec;
-            net::ip::udp::endpoint ep;
-            std::array<char, 256> d;
-            size_t size = socket.async_receive_from(net::buffer(d), ep, yield[ec]);
-            if (ec) { cerr << "Bye\n"; break; }
-            boost::string_view sv(d.data(), size);
-            std::cerr << "received " << sv << "\n";
-        }
+        //while (true) {
+        //    upnp::error_code ec;
+        //    net::ip::udp::endpoint ep;
+        //    std::array<char, 256> d;
+        //    size_t size = socket.async_receive_from(net::buffer(d), ep, yield[ec]);
+        //    if (ec) { cerr << "Bye\n"; break; }
+        //    boost::string_view sv(d.data(), size);
+        //    std::cerr << "received " << sv << "\n";
+        //}
     });
 
     ctx.run();

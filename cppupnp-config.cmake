5ba742accded434523c6b9dabae72d062927ead8
Peter Jankuliak <p.jankuliak@gmail.com>
2020-01-23 16:03:49 +0100

    Link with boost_thread only when Boost_USE_STATIC_LIBS

--- cppupnp-config.cmake
+++ cppupnp-config.cmake
@@ -2,8 +2,17 @@ cmake_minimum_required(VERSION 3.5)
 
 project(CPPUPnP VERSION 1.0.0 LANGUAGES CXX)
 
-find_package(Threads REQUIRED)
-find_package(Boost 1.71 REQUIRED COMPONENTS thread coroutine system)
+if (NOT Boost_USE_STATIC_LIBS)
+    find_package(Threads REQUIRED)
+    find_package(Boost 1.71 REQUIRED COMPONENTS coroutine system)
+    set(THREAD_LIB "Threads::Threads")
+else()
+    # When linking with static Boost, we need to link with libboost_thread.
+    find_package(Threads REQUIRED)
+    find_package(Boost 1.71 REQUIRED COMPONENTS thread coroutine system)
+    # Boost::thread adds Threads::Threads automatically
+    set(THREAD_LIB "Boost::thread")
+endif()
 
 add_library(cpp_upnp
     STATIC
@@ -23,9 +32,8 @@ target_link_libraries(cpp_upnp
     PUBLIC
         Boost::system
         Boost::coroutine
-        Boost::thread
     PRIVATE
-        Threads::Threads
+        ${THREAD_LIB}
 )
 
 target_compile_features(cpp_upnp
